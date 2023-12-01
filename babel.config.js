fda39b51237d1f1012cc4774192125788ecf06ac
TIFFANY MAINA <tiffmaina@gmail.com>
2023-12-01 08:04:53 +0300

    completed

--- babel.config.js
+++ babel.config.js
@@ -4,3 +4,10 @@ module.exports = {
     ["@babel/preset-react", { runtime: "automatic" }],
   ],
 };
+
+module.exports = function (api) {
+  return {
+    plugins: ['macros'],
+  }
+}
+
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,20 @@
-function Article ({title, date="January 1, 1970", preview}){
-    return (
-        <article>
-            <h3>{title}</h3>
-            <small>{date}</small>
-            <p>{preview}</p>
-        </article>
-    )
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  if (minutes <= 30) {
+    return( <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <small>{minutes}</small>
+      <p>{preview}</p>
+    </article>)
+  }
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <small>{minutes}</small>
+      <p>{preview}</p>
+    </article>
+  );
 }
 
 export default Article;
